4f16f9106b2db6ee635ee5dd72a7434feda6ed7b
Khaled Ahmed <khaled.mohamed.ext@dematic.com>
2020-11-15 19:51:04 +0100

    retrieving 10 latest comics from RSS feed

--- src/main/java/khaled/latestcomics/service/ComicsService.java
+++ src/main/java/khaled/latestcomics/service/ComicsService.java
@@ -1,9 +1,16 @@
 package khaled.latestcomics.service;
 
 import khaled.latestcomics.model.Comic;
+import khaled.latestcomics.util.Constants;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -14,6 +21,31 @@ import java.util.List;
 @Service
 public class ComicsService {
     public List<Comic> getLatestComics() {
-        return null;
+        RestTemplate restTemplate = new RestTemplate();
+        log.info("Calling RSS feed page to get its latest 10 comics");
+        String response = restTemplate.getForObject(Constants.RSS_TO_JSON_API_URL, String.class);
+        List<Comic> comics = parseComicsFromRss(response);
+        return comics;
+    }
+
+    private List<Comic> parseComicsFromRss(String comicsInJson) {
+        log.info("parsing json object of comics returned from RSS");
+        List<Comic> comics = new ArrayList<>();
+        try {
+            JSONObject mainJsonObject = new JSONObject(comicsInJson);
+            JSONArray comicsJsonArr = mainJsonObject.getJSONArray("items");
+            for (int i = 0; i < comicsJsonArr.length(); i++) {
+                JSONObject comicJsonObj = comicsJsonArr.getJSONObject(i);
+                String comicViewUrl = comicJsonObj.getString("link");
+                String comicTitle = comicJsonObj.getString("title");
+                String comicPicUrl = comicJsonObj.getString("thumbnail");
+                String comicDateStr = comicJsonObj.getString("pubDate").substring(0, 10);
+                LocalDate comicPublishingDate = LocalDate.parse(comicDateStr);
+                comics.add(new Comic(comicPicUrl, comicTitle, comicViewUrl, comicPublishingDate));
+            }
+        } catch (JSONException err) {
+            log.error(err.getMessage());
+        }
+        return comics;
     }
 }
---
+++ src/main/java/khaled/latestcomics/util/Constants.java
@@ -0,0 +1,11 @@
+package khaled.latestcomics.util;
+
+/**
+ * @author Khaled
+ */
+public class Constants {
+
+    private static final String RSS_SOURCE_URL = "http://feeds.feedburner.com/PoorlyDrawnLines";
+
+    public static final String RSS_TO_JSON_API_URL = "https://api.rss2json.com/v1/api.json?rss_url=" + RSS_SOURCE_URL;
+}
